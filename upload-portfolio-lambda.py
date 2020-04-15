1f3ee335d4afe269e93ca4b299c2b3e55f933bf9
Lamine Gaye <laminegaye@me.com>
2020-04-15 15:26:59 +0100

    Update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,22 +4,41 @@ import io
 import zipfile
 import mimetypes
 
-session = boto3.session.Session(profile_name='craquiest')
-s3 = session.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('lamine.gayevi.com')
-build_bucket = s3.Bucket('laminebuild.gayevi.com')
 
-# portfolio_bucket.download_file('index.html', '/tmp/index.html')
-
-# portfolio_zip = io.StringIO()
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    s3 = boto3.resource('s3')
     
-with zipfile.ZipFile(portfolio_zip) as myzip:
+    # if locally and profile needed
+    # session = boto3.session.Session(profile_name='craquiest')
+    # s3 = session.resource('s3')
+
+    portfolio_bucket = s3.Bucket('lamine.gayevi.com')
+    build_bucket = s3.Bucket('laminebuild.gayevi.com')
+    
+    
+    # portfolio_zip = io.StringIO()
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            # print(f'{nm}: {mimetypes.guess_type(nm)[0]}')
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs = {'ContentType':mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    """
+    if too much memory and want to download to disk instead
+    build_bucket.download_file('portfolio.zip', '/tmp/portfolio.zip')
+    with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        # print(f'{nm}: {mimetypes.guess_type(nm)[0]}')
         portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs = {'ContentType':mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        
+    """
+
+    print("Job done!")
+
+    return 'hello from Lambda'
+    
